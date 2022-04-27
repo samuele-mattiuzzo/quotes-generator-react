b96cc3e59857406022bae69a6a77dc9d5495a87d
Samuele Mattiuzzo <samumatt@gmail.com>
2022-04-27 14:58:43 +0100

    Populate quotes list

--- src/App.js
+++ src/App.js
@@ -3,20 +3,29 @@ import './App.css';
 
 
 const randomUrl = "https://api.quotable.io/random";
-const tagsUrl = "https://api.quotable.io/tags";
+const tagsUrl = "https://api.quotable.io/tags?sortBy=quoteCount&order=desc";
 
 function App() {
 
-  const [quotes, showQuote] = useState([]);
+  const [quotes, setQuotes] = useState([]);
+
+  const [tags, setTags] = useState([]);
 
   const getQuote = () => {
     fetch(randomUrl)
     .then((response) => response.json())
-    .then((data) => showQuote(data));
+    .then((data) => setQuotes(data));
+  }
+
+  const getTags = () => {
+    fetch(tagsUrl)
+    .then((response) => response.json())
+    .then((data) => setTags(data));
   }
 
   useEffect(() => {
     getQuote();
+    getTags();
   }, []);
 
   const nextQuote = () => {
@@ -28,6 +37,23 @@ function App() {
   return (
     <div className="main">
       <header className="content">
+
+        <select>
+        <option
+              key="all"
+              value=""
+            >
+              all
+            </option>
+          {tags.map(tag => (
+            <option
+              key={tag.name}
+              value={tag.name}
+            >
+              {tag.name} ({tag.quoteCount} quotes)
+            </option>
+          ))}
+        </select>
 
         <div class="blockquote">
           <p>{content}</p>
